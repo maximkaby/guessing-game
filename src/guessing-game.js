7d457d2293b6e2f41f2dda0047c4e3297d7d156c
maximkaby <drupalmasterpro@mail.ru>
2017-09-21 02:05:59 +0300

    Work
    100 passing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.prevMax;
+        this.min;
+        this.prevMin;
+        this.res;
+        this.range;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.res = Math.ceil((this.max + this.min)/2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
